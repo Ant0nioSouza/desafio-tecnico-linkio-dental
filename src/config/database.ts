f97c1361b86664c2b1ca5bc49cb9123a78642920
Antonio Souza <antoniosouza.dev@gmail.com>
2025-12-28 17:05:18 -0300

    chore: configure mongodb connection and environment variables

---
+++ src/config/database.ts
@@ -0,0 +1,10 @@
+import mongoose  from "mongoose";
+
+export async function connectDatabase(): Promise<void> {
+    const uri = process.env.MONGO_URI;
+
+    if (!uri) throw new Error("MONGO_URI not defined");
+
+    await mongoose.connect(uri);
+    console.log("MongoDB connected!");
+}
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
+import 'dotenv/config'
 import { app } from './app';
+import { connectDatabase } from './config/database';
 
 const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
+    await connectDatabase();
+
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     })
