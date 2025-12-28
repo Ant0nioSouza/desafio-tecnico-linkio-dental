1ebbb7023de110eec80ab30b9927edc10a021d42
Antonio Souza <antoniosouza.dev@gmail.com>
2025-12-28 20:27:37 -0300

    feat: add protected order routes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { authRoutes } from './modules/auth/auth.routes';
+import { orderRoutes } from './modules/orders/order.routes';
 
 const app = express();
 
 app.use(express.json());
 app.use('/auth', authRoutes);
+app.use('/orders', orderRoutes);
 
 app.get("/health", (_req, res) => {
     return res.status(200).json({status: 'ok'})
---
+++ src/modules/orders/order.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { OrderController } from "./order.controller";
+import { authMiddleware } from "../../middlewares/auth.middleware";
+
+const router = Router();
+
+router.post('/', authMiddleware, OrderController.create);
+router.get('/', authMiddleware, OrderController.list);
+
+export { router as orderRoutes };
